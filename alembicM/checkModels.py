5fb65a382dd4024416c5de3b6ba640002646343a
Davemag9 <103573905+Davemag9@users.noreply.github.com>
2022-10-31 23:20:42 +0200

    Add files via upload

---
+++ alembicM/checkModels.py
@@ -0,0 +1,35 @@
+from alembicM.models import Session, User, Passenger, Airport, Flight, Order
+session = Session()
+
+user = User(user_id=1, admin=False, username="Davemag", firstname="Oleh", lastname="Lozovyi",
+            email="abcd@im.a", phone_number="123456789")
+user1 = User(user_id=2, admin=False, username="NazarcO", firstname="Nazar", lastname="Lomachinkiy",
+            email="abc@im.a", phone_number="123476789")
+
+passenger = Passenger(passenger_id=1, firstname="Oleh", lastname="Lozovyi", date_of_birth='2002-09-10',
+                      passport_number="1234", user_id=1)
+passenger1 = Passenger(passenger_id=2, firstname="Nazar", lastname="Lomachinkiy", date_of_birth='2003-12-12',
+                      passport_number="1284", user_id=2)
+
+airport = Airport(airport_id=1, aiport_name="Bandera Airport", city="Lviv", country="Ukraine")
+airport1 = Airport(airport_id=2, aiport_name="Shuhevich Airport", city="Kyiv", country="Ukraine")
+
+flight = Flight(flight_id=1, flight_no="2134", datetime='2022-09-10 00:00:01', cost_of_flight=12.50,
+                num_of_seats=12, arriving_airport=airport, departing_airport=airport1)
+flight1 = Flight(flight_id=2, flight_no="2234", datetime='2022-09-10 00:00:02', cost_of_flight=120.50,
+                num_of_seats=120, arriving_airport=airport1, departing_airport=airport)
+
+order = Order(order_id=1, order_done_datetime='2022-09-07 00:00:01', status="in progress", flight_id=1, passenger_id=1)
+order1 = Order(order_id=2, order_done_datetime='2022-09-07 00:00:02', status="in progress", flight_id=2, passenger_id=2)
+
+session.add(user)
+session.add(passenger)
+session.add(airport)
+session.add(flight)
+session.add(order)
+session.add(user1)
+session.add(passenger1)
+session.add(airport1)
+session.add(flight1)
+session.add(order1)
+session.commit()
---
+++ alembicM/models.py
@@ -0,0 +1,67 @@
+from sqlalchemy import create_engine, Column, Integer, String, Enum, orm, ForeignKey, DateTime, Date, Boolean, Float
+from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
+
+DBURL = "postgresql://postgres:2580@localhost:5432/flybooking"
+engine = create_engine(DBURL)
+
+SessionFactory = sessionmaker(bind=engine)
+Session = scoped_session(SessionFactory)
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+    user_id = Column(Integer, primary_key=True)
+    admin = Column(Boolean, default=False, nullable=False)
+    username = Column(String(45), nullable=False, unique=True)
+    firstname = Column(String(20), nullable=False)
+    lastname = Column(String(20), nullable=False)
+    email = Column(String(45), nullable=False, unique=True)
+    phone_number = Column(String(15), unique=True)
+
+
+class Passenger(Base):
+    __tablename__ = "passenger"
+    passenger_id = Column(Integer, primary_key=True)
+    firstname = Column(String(20), nullable=False)
+    lastname = Column(String(20), nullable=False)
+    date_of_birth = Column(Date, nullable=False)
+    passport_number = Column(String(10), nullable=False, unique=True)
+    user_id = Column(Integer, ForeignKey(User.user_id))
+    user = orm.relationship(User, backref="passenger")
+
+
+class Airport(Base):  # bandera
+    __tablename__ = "airport"
+
+    airport_id = Column(Integer, primary_key=True)
+    aiport_name = Column(String(45), nullable=False, unique=True)
+    city = Column(String(45), nullable=False)
+    country = Column(String(45), nullable=False)
+
+
+class Flight(Base):  # chornovil
+    __tablename__ = "flight"
+
+    flight_id = Column(Integer, primary_key=True)
+    flight_no = Column(String(45), nullable=False, unique=True)
+    datetime = Column(DateTime, nullable=False)
+    cost_of_flight = Column(Float, nullable=False)
+    num_of_seats = Column(Integer, nullable=False)
+
+    arriving_airport_id = Column(Integer, ForeignKey(Airport.airport_id))
+    arriving_airport = orm.relationship(Airport, foreign_keys=[arriving_airport_id], backref="flight_from", lazy="joined")
+    departing_airport_id = Column(Integer, ForeignKey(Airport.airport_id))
+    departing_airport = orm.relationship(Airport, foreign_keys=[departing_airport_id], backref="flight_to", lazy="joined")
+
+class Order(Base):
+    __tablename__ = "order"
+
+    order_id = Column(Integer, primary_key=True)
+    order_done_datetime = Column(DateTime, nullable=False)
+    status = Column(String(45), nullable=False)
+    flight_id = Column(Integer, ForeignKey(Flight.flight_id))
+    flight = orm.relationship(Flight, backref="order")
+    passenger_id = Column(Integer, ForeignKey(Passenger.passenger_id))
+    passenger = orm.relationship(Passenger, backref="order")
